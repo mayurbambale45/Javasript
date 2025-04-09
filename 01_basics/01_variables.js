e7a64ad7fc10aac40d037704f73ed26fe537c528
mayurbambale45 <161823186+mayurbambale45@users.noreply.github.com>
2025-04-09 04:22:51 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+console.log("Hello World!");
+
+const PRN = 23510010
+let city = "sangli"
+var fname = "mayur"
+email = "mayur@gmail.com"
+
+console.table([PRN, city, fname, email])
+console.log(email)
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
