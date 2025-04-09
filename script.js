74c25cc7f75f9af7c401779d4aeb99c5b1e8a8bf
mayurbambale45 <161823186+mayurbambale45@users.noreply.github.com>
2025-04-09 09:54:09 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello World!");
-
-const PRN = 23510010
-let city = "sangli"
-var fname = "mayur"
-email = "mayur@gmail.com"
-
-console.table([PRN, city, fname, email])
-console.log(email)
