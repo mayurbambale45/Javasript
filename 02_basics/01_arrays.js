63639419678c439fb0438de769d2d8eb63c4debf
mayurbambale45 <161823186+mayurbambale45@users.noreply.github.com>
2025-04-11 05:23:08 +0000

    arrays part-1 done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,41 @@
+// Arrays =>
+
+    // Declaration of Array => 
+    const myArr = [1, 2, 3, 4, 5, 6]
+    const myArray = ['a', 'b', 'c', 'd', 'e'] // => inside array values called elements
+    const myArray2 = new Array(1, 2, 3, 4, 5)
+
+    // console.log(myArray[2])
+
+//Array Methods =>
+    myArr.push(7)
+    myArr.push(8)
+    myArr.push(10)
+    // console.log(myArr)
+    myArr.pop()         // => No arguments req. Remove last value
+    // console.log(myArr)
+
+    myArr.unshift(9)    // => Insert at starting of array (index[0]). Shift all the next values (not used mostly)
+    // console.log(myArr)
+    myArr.shift()       // => Remove element from starting (at index[0])
+    // console.log(myArr)
+
+    // console.log(myArr.includes(9))      // => Check value is included or not
+    // console.log(myArr.indexOf(7))       // => Find index of given value    
+    // console.log(myArr.length)           // => Find length of Array
+
+const newArray = myArr.join()
+
+// console.log(myArr)       // => Type is Object
+// console.log(newArray)    // => Type is String
+
+// Slice & Splice => ****************** Most important for Interview **************************
+    console.log("A ", myArr);
+
+    const myA1 = myArr.slice(1, 3)      // => Return section of [ )
+    console.log(myA1)
+    console.log("B ", myArr);
+
+    const myA2 = myArr.splice(1, 3)      // => Return section of [ ] & selected elements are remove from original array. 
+    console.log(myA1)
+    console.log("C ", myArr);
